22769400eb11ecf55180fdb1a520f39ea8bc8dcb
dikoshelev <dmitrykoshelev@0110111.local>
2015-10-14 13:22:14 +0300

    button animation correction

--- animation.js
+++ animation.js
@@ -1,11 +1,22 @@
-$(document).ready(function(){
-  $('.button').click(function(){
-    $('.button').toggleClass('button2');
-    // $('.button2').css("background-color", "transparent");
-    $('.button2').text('done');
+$(document).ready(function() {
+  $('.button').click(function() {
+
+    var _this = $(this);
+    if (_this.hasClass('button2')) {
+      $('.button2').addClass('button');
+    } else {
+      $('.button').toggleClass('button2');
+
+      setTimeout(function() {
+        _this.text('done.').toggleClass('button3');
+
+      }, 500);
+
+    }
+
   });
 
-  $('h1').click(function(){
+  $('h1').click(function() {
     $('h1').toggleClass('text');
   });
 
